ad4b238143a63f0c9ce857655b7cfd9cdabd345e
fernanda <molina.fefe@gmail.com>
2024-03-18 22:39:51 -0300

    Aula3

--- Aula 3/Aula3.py
+++ Aula 3/Aula3.py
@@ -1,33 +1,27 @@
-#1
+# 1
+import velocidade as velocidade
 
 # velocidade = int(input("Velocidade do carro: "))
 
 if velocidade > 80:
     multa = (velocidade - 80) * 5
     print(f"Você foi multado por excesso de velocidade, valor total a pagar R$ {multa:.2f}")
+else:
+    print("Velocidade permitida.")
+# 2
 
-#2
 
-#3.
-
+# 3.
+  
 v_salario = float(input("Valor do salário: "))
 
-if(v_salario > 1250.00):
-   porcentual = 10
+if v_salario > 1250.00:
+    porcentual = 10
 else:
     porcentual = 15
 
-
-v_porcentual = porcentual/100
+v_porcentual = porcentual / 100
 aumento = v_porcentual * v_salario
 total = v_salario + aumento
 
-print(f"A porcentagem de aumento é de {porcentual}% e o valor do novo salario é de R$ {total:.2f}")
-
-
-
-
-
-
-
-
+print(f"A porcentagem de aumento é de {porcentual}% e o valor do novo salário é de R$ {total:.2f}")
