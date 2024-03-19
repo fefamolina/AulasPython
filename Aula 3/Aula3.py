870ced57980419e3ea65866eaad0b70d1972222c
fernanda <molina.fefe@gmail.com>
2024-03-18 22:03:33 -0300

    Aula3

---
+++ Aula 3/Aula3.py
@@ -0,0 +1,10 @@
+#O comando if é utilizado quando precisamos decidir se um trecho do programa deve ou não ser executado, com base no resultado de uma condição.
+
+#Exemplo
+
+media_final = int(input("Digite a sua média: "))
+
+if media_final >= 60:
+    print("Aprovado")
+else:
+    print("Reprovado")
