9408aa5ab9dd116a3d5a08a5218653e66b8c129b
fernanda <molina.fefe@gmail.com>
2024-03-18 22:49:35 -0300

    Aula3

--- Aula 3/Aula3.py
+++ Aula 3/Aula3.py
@@ -1,27 +1,45 @@
 # 1
-import velocidade as velocidade
-
 # velocidade = int(input("Velocidade do carro: "))
-
-if velocidade > 80:
-    multa = (velocidade - 80) * 5
-    print(f"Você foi multado por excesso de velocidade, valor total a pagar R$ {multa:.2f}")
-else:
-    print("Velocidade permitida.")
+#
+# if velocidade > 80:
+#     multa = (velocidade - 80) * 5
+#     print(f"Você foi multado por excesso de velocidade, valor total a pagar R$ {multa:.2f}")
+# else:
+#     print("Velocidade permitida.")
 # 2
 
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+
+maior = n1
+menor = n1
+
+if n2 > n1 and n2 > n3:
+    maior = n2
+
+if n3 > n1 and n3 > n1:
+    maior = n3
+
+if n2 < n1 and n2 < n1:
+    menor = n2
+
+if n3 < n1 and n3 < n1:
+    menor = n3
+
+print(f"O número maior é {maior} e o menor é {menor}")
 
 # 3.
   
-v_salario = float(input("Valor do salário: "))
-
-if v_salario > 1250.00:
-    porcentual = 10
-else:
-    porcentual = 15
-
-v_porcentual = porcentual / 100
-aumento = v_porcentual * v_salario
-total = v_salario + aumento
-
-print(f"A porcentagem de aumento é de {porcentual}% e o valor do novo salário é de R$ {total:.2f}")
+# v_salario = float(input("Valor do salário: "))
+#
+# if v_salario > 1250.00:
+#     porcentual = 10
+# else:
+#     porcentual = 15
+#
+# v_porcentual = porcentual / 100
+# aumento = v_porcentual * v_salario
+# total = v_salario + aumento
+#
+# print(f"A porcentagem de aumento é de {porcentual}% e o valor do novo salário é de R$ {total:.2f}")
