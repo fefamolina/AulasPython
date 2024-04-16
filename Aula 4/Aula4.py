0b04dfb3e153581f7dc6d6d1a2bbd656e8ad6a67
fefamolina <molina.fefe@gmail.com>
2024-04-15 21:54:44 -0300

    Aula4

--- Aula 4/Aula4.py
+++ Aula 4/Aula4.py
@@ -1,15 +1,28 @@
-pontos = 0
-questao = 1
+# #Exemplo1
+# #
+#
+# pontos = 0
+# questao = 1
+#
+# while questao <= 3:
+#     resposta = input(f"Resposta da questão {questao}: ")
+#
+#     if questao == 1 and resposta == "b":
+#         pontos = pontos + 1
+#     elif questao == 2 and resposta == "a":
+#         pontos = pontos + 1
+#     elif questao == 3 and resposta == "d":
+#         pontos = pontos + 1
+#     questao = questao + 1
+#
+# print(f"O aluno fez {pontos} ponto(s)")
 
-while questao <= 3:
-    resposta = input(f"Resposta da questão {questao}: ")
+n = 1
+soma = 0
 
-    if questao == 1 and resposta == "b":
-        pontos = pontos + 1
-    elif questao == 2 and resposta == "a":
-        pontos = pontos + 1
-    elif questao == 3 and resposta == "d":
-        pontos = pontos + 1
-    questao = questao + 1
+while n <= 10:
+    x = float(input(f"Digite o número {n}: "))
+    soma = soma + x
+    n = n + 1
 
-print(f"O aluno fez {pontos} ponto(s)")
+print(f"Soma = {soma:.2f}")
