39991c93c454bc77c54de173fb27d54d4b3782cc
fefamolina <molina.fefe@gmail.com>
2024-04-15 22:24:00 -0300

    Exercicios

--- Aula 4/Exercicios.py
+++ Aula 4/Exercicios.py
@@ -26,8 +26,56 @@
 # digitado pelo usuário que mostre apenas os números
 # ímpares
 
-num = int(input("Digite um número: "))
-x = 0
-    while x <= num:
-    print(x)
-    x += 1
+# num = int(input("Digite um número: "))
+# x = 1
+# while x <= num:
+#     print(x)
+#     x = x + 2
+#
+# 5. Faça um programa para escrever os 10 primeiros
+# múltiplos de 3
+
+# num = 30
+# x = 3
+# while x <= num:
+#     print(x)
+#     x = x + 3
+
+# 6. Faça um programa para exibir os resultados de uma
+# tabuada no formato: 2 x 1 = 2, 2 x 2 = 4, ...
+# 7. Modifique o programa interior de forma que o usuário
+# também digite o início e o fim da tabuada, em vez de
+# começar com 1 e 10
+
+# n = 2
+# inicio = int(input("De: "))
+# fim = int(input("Até: "))
+# x = inicio
+#
+# while x <= fim:
+#     print(f"{n} x {x} = {n*x}")
+#     x = x + 1
+#
+# 8. Escreva um programa que pergunte o depósito inicial
+# e a taxa de juros de uma poupança. Exiba os valores
+# mês a mês para os 24 primeiros meses. Escreva o total
+# do ganho com juros no período
+
+#
+# 9. Altere o programa anterior de forma a perguntar
+# também o valor depositado mensalmente. Esse valor
+# será depositado no início de cada mês e você deve
+# considerá-lo para o cálculo de juros do mês seguinte
+
+deposito = float(input("Deposito inicial: "))
+taxa = float(input("Taxa de juros: "))
+mensalmente = float(input("Valor depositado mensalmente: "))
+mes = 1
+saldo = deposito
+
+while mes <= 24:
+    saldo = saldo + (saldo * (taxa /100) + mensalmente)
+    print(f"O saldo do mês de {mes} é de R$ {saldo:.2f}")
+    mes = mes + 1
+
+print(f"O ganho com juros é de R$ {saldo-deposito:.2f}")
