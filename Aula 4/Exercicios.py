a1e39bc69df168fcccdae4c522f7a4a95cf849e7
Fernanda Molina <94088509+fefamolina@users.noreply.github.com>
2024-04-22 15:18:46 -0300

    Update Exercicios.py
    Aula 4

--- Aula 4/Exercicios.py
+++ Aula 4/Exercicios.py
@@ -67,15 +67,61 @@
 # será depositado no início de cada mês e você deve
 # considerá-lo para o cálculo de juros do mês seguinte
 
-deposito = float(input("Deposito inicial: "))
-taxa = float(input("Taxa de juros: "))
-mensalmente = float(input("Valor depositado mensalmente: "))
-mes = 1
-saldo = deposito
+# deposito = float(input("Deposito inicial: "))
+# taxa = float(input("Taxa de juros: "))
+# mensalmente = float(input("Valor depositado mensalmente: "))
+# mes = 1
+# saldo = deposito
 
-while mes <= 24:
-    saldo = saldo + (saldo * (taxa /100) + mensalmente)
-    print(f"O saldo do mês de {mes} é de R$ {saldo:.2f}")
-    mes = mes + 1
+# while mes <= 24:
+#     saldo = saldo + (saldo * (taxa /100) + mensalmente)
+#     print(f"O saldo do mês de {mes} é de R$ {saldo:.2f}")
+#     mes = mes + 1
 
-print(f"O ganho com juros é de R$ {saldo-deposito:.2f}")
+# print(f"O ganho com juros é de R$ {saldo-deposito:.2f}")
+
+# 10. Escreva um programa que leia números inteiros do teclado. O
+# programa deve ler os números até que o usuário digite 0 (zero). No
+# final da execução, exiba a quantidade de números digitados, assim
+# como a soma e a média aritmética.
+    
+# soma = 0
+# quantidade = 0
+# while True:
+# num = int(input("Digite um número inteiro: "))
+# if num == 0:
+# break
+# soma = soma + num
+# quantidade += 1
+# print("Quantidade de números digitados:",quantidade)
+# print("Soma =",soma)
+# print(f"Média = {(soma/quantidade):.2f}")
+
+# 11. Escreva um programa para controlar uma pequena máquina registradora. Você deve solicitar
+# ao usuário que digite o código do produto e a quantidade comprada. Utilize a tabela de códigos a
+# seguir para obter o preço de cada produto:
+# Seu programa deve exibir o total das compras depois que o usuário digitar 0. Qualquer outro
+# código deve gerar a mensagem de erro “Código inválido”.
+    
+# apagar = 0
+# while True:
+# codigo = int(input("Código do produto (0 para sair): "))
+# preco = 0
+# if codigo == 0:
+# break
+# elif codigo == 1:
+# preco = 0.5
+# elif codigo == 2:
+# preco = 1
+# elif codigo == 3:
+# preco = 4
+# elif codigo == 5:
+# preco = 7
+# elif codigo == 9:
+# preco = 8
+# else:
+# print("Código inválido!")
+# if preco != 0:
+# quantidade = int(input("Quantidade: "))
+# apagar = apagar+ (preco * quantidade)
+# print(f"Total a pagar R${apagar:.2f}")
