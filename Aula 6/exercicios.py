c3146adaac3c0f2d36b4bd20da7f17009d147dff
labsfiap <molina.fefe@gmail.com>
2024-08-12 22:39:14 -0300

    Aula 06 - exemplos

--- Aula 6/exercicios.py
+++ Aula 6/exercicios.py
@@ -1,11 +1,19 @@
 cadastro = {"48769878998": ["Fernanda", 2003, "São Paulo", "SP"],
             "56987415632": ["Geovana", 2005, "São Paulo", "SP"],
             "56235469874": ["Ana Clara", 2004, "São Paulo", "SP"]}
+
+for chave, valores in cadastro.items():
+    print("Nome: ", valores[0])
+    print("CPF: ", chave)
+    print("Ano de nascimento: ", valores[1])
+    print(f"Cidade e Estado de nascimento: , {valores[2]}/{valores[3]}\n")
+
 while True:
-    pessoa = input("Digite o CPF ou fim pra terminar: ")
-    if pessoa == "fim" or "Fim":
+    pessoa = input("Digite o CPF ou fim pra terminar: ").lower()
+    if pessoa == "fim":
         break
-    if pessoa in cadastro:
+    elif pessoa in cadastro:
         print(cadastro[pessoa])
+        print(cadastro[pessoa][1])
     else:
         print("CPF não encontrado.")
